99597b17f53389c0fae0eaa6c0f9804ad646fce1
Miguel <Miguel@DESKTOP-AIUBK8A>
2025-11-25 02:07:01 -0400

    Forgot to commit the common interface for File and Folder

---
+++ FileSystem_Sim/src/main/java/OS_Structures/DiskElement.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package OS_Structures;
+
+/**
+ *
+ * @author Miguel
+ */
+public interface DiskElement {
+    public String getName();
+    public Folder getParent();
+}
